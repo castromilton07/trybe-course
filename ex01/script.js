74027da67ad7b9987be60141d83a0b2cfd264400
Milton Castro <castro.milton07@gmail.com>
2021-04-29 15:22:08 -0300

    01 - Reorganiza a estrutura da branch em pastas e inclui o 2ndo exemplo

---
+++ ex01/script.js
@@ -0,0 +1,7 @@
+// console.log(document.getElementById('teste1'));
+
+// console.log(document.getElementById('teste1').innerHTML);
+
+// console.log(document.getElementById('teste1').innerText);
+
+document.getElementById('teste1').innerText = 'Aprendizados da aula de hoje';
---
+++ ex02/script.js
@@ -0,0 +1,8 @@
+var paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+var title = document.getElementById('page-title');
+title.innerText = 'O Poderoso Chefão';
+var secondParagraph = document.getElementById('second-paragraph');
+secondParagraph.innerText = 'É um filme que conta a história de uma família (Os Corleone) que coordenava a máfia italiana nos EUA e era cheia de valores éticos e morais ligados a preservação da família, além possuir um código de conduta muito bem estabelecido nos relacionamentos pessoais e comerciais dos seus integrantes.';
+var subtitle = document.getElementById('subtitle');
+subtitle.innerText = 'Trilogia';
