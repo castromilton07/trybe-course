6ea71e45a0a3f928870926c21b424e6b69361cb8
Milton Castro <castro.milton07@gmail.com>
2021-04-29 20:43:12 -0300

    05 - Adiciona o exemplo 06

---
+++ ex06/script.js
@@ -0,0 +1,8 @@
+let querySelecDona = document.querySelector('#Dona');
+querySelecDona.innerText = 'Midia Liberdade';
+let querySelecP = document.querySelector('.pilotos-sem-titulo p');
+querySelecP.innerHTML += ' (O grande)';
+let querySelecSemTituloF1 = document.querySelector('.pilotos-sem-titulo .piloto-f1-atual');
+querySelecSemTituloF1.innerHTML += ' [The Famous]';
+
+document.querySelector('.pilotos-sem-titulo span').innerHTML += '{The Faster}';
