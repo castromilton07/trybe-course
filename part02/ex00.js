cedb797cb6941fc16c94d68ad50cd52193374b67
Milton Castro <castro.milton07@gmail.com>
2021-05-02 21:09:11 -0300

    02 - Adiciona os arquivos referente aos exemplos 00 e 01 da Parte 02

---
+++ part02/ex00.js
@@ -0,0 +1,8 @@
+let cars = ['Saab', 'Volvo', 'BMW'];
+console.log(cars[0]);
+console.log(cars[1]);
+console.log(cars[2]);
+
+for (let index = 0; index < cars.length; index += 1) {
+    console.log(cars[index]);
+}
