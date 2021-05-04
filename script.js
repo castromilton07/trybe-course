58be7aea86bbc2c73fcba20eeade54e39b1ce6b6
Milton Castro <castro.milton07@gmail.com>
2021-05-04 00:19:58 -0300

    03 - Adiciona alteraçoes que atendem aos requisitos dos ex. 3, 4, 4.1 e 5

--- script.js
+++ script.js
@@ -13,8 +13,6 @@ redirecione para alguma página;
 
 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 a cor do mesmo;
-
-Segue abaixo um exemplo do uso de event.target:
 */
 
 const divUm = document.getElementById('divUm');
@@ -26,7 +24,7 @@ const myWebpage = document.getElementById('mySpotrybefy');
 divDois.className = 'techa';
 
 
-// Ex. 01
+// Ex. 2 e 2.1
 function addClassTech(event) {
     if (event.target.id === 'divUm') {
         event.target.className = 'tech';
@@ -49,14 +47,59 @@ divUm.addEventListener('click', addClassTech);
 divDois.addEventListener('click', addClassTech);
 divTres.addEventListener('click', addClassTech);
 
-function resetText(event) {
-  // O Event é passado como um parâmetro para a função.
-  event.target.innerText = 'Opção reiniciada';
-  // O event possui várias propriedades, porém a mais usada é o event.target,
-  // que retorna o objeto que disparou o evento.
+// Ex. 3
+// Opção 1 - Utilizando Atributo via HTML OnInput
+/*input.setAttribute('oninput', 'editClassTech()');
+function editClassTech() {
+    let value = document.getElementById('input').value;
+    if (value !== null) {
+        let verifyClassTech = document.getElementsByClassName('tech')[0];
+        verifyClassTech.innerHTML = value;
+    }
+}*/
+// Opção 2 - Utilizando o Escultador de Eventos (EventListener)
+function editClassTech(event) {
+    if (event.target.value !== null) {
+        let verifyClassTech = document.getElementsByClassName('tech')[0];
+        verifyClassTech.innerHTML = event.target.value;
+    }
 }
+input.addEventListener('input', editClassTech);
 
+// Ex. 4 e 4.1
+function redirectToUrl(event) {
+    location.href = "https://github.com/castromilton07";
+}
+myWebpage.addEventListener('dblclick', redirectToUrl);
+
+// Ex. 5
+function changeColorOver(event) {
+    event.target.style.color = 'rgb(46, 193, 140)';
+}
+function changeColorOut(event) {
+    event.target.style.color = 'rgb(255, 255, 255)';
+}
+myWebpage.addEventListener('mouseover', changeColorOver);
+myWebpage.addEventListener('mouseout', changeColorOut);
+
+// Segue abaixo um exemplo do uso de event.target:
+function resetText(event) {
+    // O Event é passado como um parâmetro para a função.
+    if (event.target.id === 'divUm') {
+        event.target.innerText = 'Aqui está a primeira tecnologia que mais gostei.';
+    }
+    if (event.target.id === 'divDois') {
+        event.target.innerText = 'Aqui está a segunda tecnologia que mais gostei.';
+    }
+    if (event.target.id === 'divTres') {
+        event.target.innerText = 'Aqui está a terceira tecnologia que mais gostei.';
+    }
+    // O event possui várias propriedades, porém a mais usada é o event.target,
+    // que retorna o objeto que disparou o evento.
+}
 divUm.addEventListener('dblclick', resetText);
+divDois.addEventListener('dblclick', resetText);
+divTres.addEventListener('dblclick', resetText);
 // Não precisa passar o parâmetro dentro do addEventListener. O próprio
 // navegador fará esse trabalho por você, não é legal? Desse jeito, o
 // event.target na nossa função retornará o objeto 'divUm'.
