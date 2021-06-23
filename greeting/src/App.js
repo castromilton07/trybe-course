da57b068a39e4c042bf6cbd8969b93bd33f899ca
Milton Castro <castro.milton07@gmail.com>
2021-06-22 23:39:35 -0300

    01 - Adiciona os arquivos ref ao App React dos exemplos iniciais

---
+++ greeting/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Component from './Component';
+
+function App() {
+  return (
+    <div>
+      <span>Hello Word!</span>
+      <Component />
+    </div>
+  );
+}
+
+export default App;
---
+++ greeting/src/Component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Component extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Milton Castro</h1>
+        <p>Pessoa estudante da Turma 12 (Patursos)</p>
+      </div>
+    );
+  };
+}
+
+export default Component;
