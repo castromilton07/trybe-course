8d9ff2b931e45ff4999c19aa95c3ebe5e3c5965b
Milton Castro <castro.milton07@gmail.com>
2021-06-23 01:47:42 -0300

    02 - Adiciona o restantes dos exemplos referente ao bloco 11.2

--- fixation-exercises-11-2/src/index.js
+++ fixation-exercises-11-2/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import Image from './Image';
+// import Coldplay from './Coldplay';
+// import User from './User';
+// import Customer from './Customer';
+// import Shopping from './Shopping';
+import Greeting from './Greeting';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText="Cute cat staring" /> */}
+    {/* <Coldplay /> */}
+    {/* <User /> */}
+    {/* <Customer /> */}
+    {/* <Shopping /> */}
+    <Greeting name="Milton" lastName="Castro" />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- greeting/src/App.js
+++ greeting/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Component from './Component';
+import Greeting from './Greeting';
 
-function App() {
-  return (
-    <div>
-      <span>Hello Word!</span>
-      <Component />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <span>Hello Word!</span>
+        <Component />
+        <Greeting name="Milton" lastName="Castro" />
+      </div>
+    );
+  };
 }
 
 export default App;
