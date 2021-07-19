bd8ec33cf72cbfe5ceb5775a3a357ac3671f127c
Milton Castro <castro.milton07@gmail.com>
2021-07-19 13:46:10 -0300

    Adiciona o primeiro exemplo de RTL

--- fixation-exercises-15.1/src/App.test.js
+++ fixation-exercises-15.1/src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Verifica que há um campo input de email na tela', () => {
+  // Acessa os elementos da sua tela
+  const { getByLabelText } = render(<App />);
+  const inputEmail = getByLabelText('Email');
+  // Fezer seu teste
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail.type).toBe('email');
 });
+
+// Acessa os elementos da sua tela
+
+  // Interagir com eles (se houver necessidade)
+
+  // Fezer seu teste
