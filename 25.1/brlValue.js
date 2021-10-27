f8ce67de247159c6fc76c874f970cbf612679cce
castromilton07 <castro.milton07@gmail.com>
2021-10-27 18:40:56 -0300

    ADD-EXAMPLE-OF-COURSE-25.1

---
+++ 25.1/brlValue.js
@@ -0,0 +1,3 @@
+const brl = 5.37;
+const usdToBrl = (valueInUsd) => valueInUsd * brl;
+module.exports = { brl, usdToBrl };
---
+++ 25.1/usdConvert.js
@@ -0,0 +1,4 @@
+const { brl, usdToBrl } = require('./brlValue');
+
+console.log(`Valor do dólar: ${brl}`);
+console.log(`10 dólares em reais: ${usdToBrl(10)}`);
